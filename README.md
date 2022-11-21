e160007aa317d6acd9ad2c2ef2ded6d72c6b6438
Johannes Naylor <jonaylor89@gmail.com>
2022-11-21 21:22:01 +0100

    trying it out

--- README.md
+++ README.md
@@ -32,3 +32,13 @@ You can check out [the Next.js GitHub repository](https://github.com/vercel/next
 The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
 
 Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+
+----------
+
+TODO
+
+* Single page app with audius username input (also spotify and soundcloud) 
+* Serverless function to generate the image (https://vercel.com/docs/concepts/functions/edge-functions/og-image-generation) (Johannes)
+* Data layer for Audius API (Thomas)
+    * What data can we get?
+    * What data would be useful to have in the business card?
---
+++ pages/api/og.tsx
@@ -0,0 +1,58 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { ImageResponse } from '@vercel/og'
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+export const config = {
+    runtime: 'experimental-edge',
+}
+
+const IMAGE_WIDTH = 1200
+const IMAGE_HEIGHT = 628
+
+export default function handler(
+  req: NextApiRequest,
+) {
+  // if (req.url === undefined) {
+  //   return new Response(`No url defined`, {
+  //     status: 500,
+  //   });
+  // }
+
+  //   const { pathname, searchParams } = new URL(req.url)
+  //   const [, , username] = pathname.match(/^\/(api\/image|i)\/([^\/]+)/) || []
+  //   const dark = searchParams.has('dark')
+  //   const removeLink = searchParams.has('removeLink')
+  //   const noBorder = searchParams.has('noBorder')
+  
+  //   if (!username) {
+  //     return new Response(`No username defined`, {
+  //       status: 401,
+  //     });
+  //   }
+
+  return new ImageResponse(
+    (
+      <div
+      style={{
+        fontSize: 100,
+        color: 'black',
+        background: 'white',
+        width: '100%',
+        height: '100%',
+        padding: '50px 200px',
+        textAlign: 'center',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      Hey Thomas 👋, 🌎
+    </div>
+    ),
+    {
+      width: IMAGE_WIDTH,
+      height: IMAGE_HEIGHT,
+      emoji: 'twemoji',
+      headers: { 'cache-control': 'public, max-age=60' },
+    }
+  )
+}
