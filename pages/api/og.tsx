bd6437b549e7b1b97af012fea5d2065fce82548d
Johannes Naylor <jonaylor89@gmail.com>
2022-11-21 22:55:25 +0100

    error response

--- pages/api/og.tsx
+++ pages/api/og.tsx
@@ -13,9 +13,7 @@ export default function handler(
   req: NextApiRequest,
 ) {
   if (req.url === undefined) {
-    return new Response(`No url defined`, {
-      status: 500,
-    });
+    return errorResponse('something went wrong')
   }
 
   const { searchParams } = new URL(req.url)
@@ -27,14 +25,11 @@ export default function handler(
   // const removeLink = searchParams.has('removeLink')
   // const noBorder = searchParams.has('noBorder')
 
-  if (!username || username === '') {
-    return new Response(`No username defined`, {
-      status: 401,
-    });
+  if (username === '') {
+    return errorResponse('username cannot be empty')
   }
 
   try {
-
     return new ImageResponse(
       (
         <div
@@ -63,9 +58,24 @@ export default function handler(
     )
   } catch (e: any) {
     console.log(e)
-    return new Response('error generating the image', {
-      status: 500
-    })
+    return errorResponse(e.message)
   }
+}
 
+function errorResponse(message: string): ImageResponse {
+  return new ImageResponse(
+    (
+      <div tw="h-full w-full flex flex-col p-4 items-center justify-center">
+        <div tw="flex flex-col items-stretch rounded-xl shadow-2xl min-w-1/2">
+          <div tw="text-3xl bg-red-600 text-white px-4 py-2 rounded-t-xl">
+            Error
+          </div>
+          <div tw="text-xl border border-red-600 p-4 rounded-b-xl bg-white">
+            {message}
+          </div>
+        </div>
+      </div>
+    ),
+    { width: IMAGE_WIDTH, height: IMAGE_HEIGHT }
+  )
 }
